dabde73c01f4250de56544c9f574a9ba90a8ed47
makartashik <wowmalish@gmail.com>
2018-06-26 03:54:20 +0300

    форма не будет отправлена с пустыми полями

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 var search = document.querySelector(".search-section-container");
 var button = search.querySelector(".search-section-button");
-var form = document.querySelector(".modal-search-form");
+var blockForm = document.querySelector(".modal-search-form");
+var form = document.querySelector(".search-form");
+var arrival = form.querySelector("[name=arrival-date]");
+var depart = form.querySelector("[name=departure-date]");
+var adults = form.querySelector("[name=adults-amount]");
+var child = form.querySelector("[name=child-amount]");
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
-  form.classList.toggle("modal-form-show");
+  blockForm.classList.toggle("modal-form-show");
+    }
+);
+
+form.addEventListener("submit", function (evt) {
+  if (!arrival.value || !depart.value || !adults.value || !child.value) {
+      evt.preventDefault();
+      console.log("Заполните, пожалуйста, все поля");
+    }
 });
