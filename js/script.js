279a9b7c855011be6f20c691b0c1123c3447443f
makartashik <wowmalish@gmail.com>
2018-06-27 14:02:35 +0300

    преположение, как оживить кнопки - +

--- js/script.js
+++ js/script.js
@@ -10,6 +10,12 @@ var isStorageSupport = true;
 var storageAd = "";
 var storageCh = "";
 
+var less = document.querySelectorAll(".less-amount-button");
+var more = document.querySelectorAll(".more-amount-button");
+var ad = adults.value;
+var ch = child.value;
+
+
 try {
     storageAd = localStorage.getItem("adults");
     storageCh = localStorage.getItem("child");
@@ -45,5 +51,29 @@ form.addEventListener("submit", function (evt) {
         if (isStorageSupport) {
           localStorage.setItem("child", child.value);
       }
+    }
+});
+
+less.addEventListener("click", function (evt) {
+    if (ad >= 2) {
+        ad.value = parseInt("ad", 10) - 1;
+    }
+    if (ch >= 1) {
+        ad.value = parseInt("ch", 10) - 1;
+    }
+    else {
+      evt.preventDefault();
+    }
+});
+
+more.addEventListener("click", function (evt) {
+    if (ad >= 0) {
+        ad.value = parseInt("ad", 10) + 1;
+    }
+    if (ch >= 0) {
+        ad.value = parseInt("ch", 10) + 1;
+    }
+    else {
+      evt.preventDefault();
     }
 });
